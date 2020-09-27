df4f2118b878a7fec720d90e859c22687d373ed5
Miles Bradt <smbradtmichael@gmail.com>
2020-09-26 20:47:12 -0700

    refactor complete

--- public/scripts/connectToChat.js
+++ public/scripts/connectToChat.js
@@ -1,5 +1,4 @@
 function connectToChat(channel) {
-
     // Create WebSocket connection.
     const socket = new WebSocket('ws://localhost:8080');
 
@@ -9,67 +8,83 @@ function connectToChat(channel) {
         socket.send(channel)
     });
 
-    const chat = document.getElementById('chat');
-
     // Listen for messages
     socket.addEventListener('message', function (event) {
-        const chatLog = JSON.parse(event.data);
-
-        shouldScroll = chat.scrollTop + chat.clientHeight === chat.scrollHeight;
-
-        const divChannel = document.createElement("div")
-        divChannel.id = "channelLine"
-        divChannel.className = "channelLines"
-        divChannel.textContent = chatLog.channel + "'s chat"
-        document.getElementById("chat").appendChild(divChannel)
-
-        const divChat = document.createElement("div")
-        divChat.id = "chatLine"
-        divChat.className = "chatLines"
-        document.getElementById("chat").appendChild(divChat);
-
-        const usernameSpan = document.createElement("span");
-        usernameSpan.id = chatLog.id;
-        usernameSpan.textContent = chatLog.username;
-        usernameSpan.style.color = chatLog.color;
-        usernameSpan.style.fontWeight = "bold";
-        divChat.appendChild(usernameSpan)
-        console.log(usernameSpan)
-
-        const messageSpan = document.createElement("span");
-        messageSpan.className = "messages";
-        messageSpan.style.color = "#979799";
-        messageSpan.style.fontWeight = "normal";
-        const messages = chatLog.message
-        const messagesArray = [];
-
-        for (const i in messages) {
-            console.log("each message: " + messages[i])
-            if(messages[i].type === "emote") {
-                messagesArray.push("<img src=https://static-cdn.jtvnw.net/emoticons/v1/" + messages[i].id + "/1.0></img>")
-            }
-            if(messages[i].type === "text") {
-                messagesArray.push(messages[i].text)
-            }
-        }
-
-        messageSpan.innerHTML = ": " + messagesArray.join('')
-        console.log(chatLog.message)
-        divChat.appendChild(messageSpan)
-
-        
-
-        if (!shouldScroll) {
-            scrollToBottom();
-        }
-
-        console.log('Message from server ', chatLog);
+        postToDOM(event)
     });
-
-
 
 }
 
-function scrollToBottom(divChat) {
+function scrollToBottom(chat) {
     chat.scrollTop = chat.scrollHeight;
+}
+
+function postToDOM(event) {
+    const chatLog = JSON.parse(event.data);
+    const chat = document.getElementById('chat');
+
+    let shouldScroll = chat.scrollTop + chat.clientHeight === chat.scrollHeight;
+
+    createChannelLine(chatLog)
+    let chatLine = createChatLine()
+    createUserNameSpan(chatLog, chatLine)
+    let messageSpan = createMessageSpan(chatLog)
+    
+    chatLine.appendChild(messageSpan)
+
+    if (!shouldScroll) {
+        scrollToBottom(chat);
+    }
+}
+
+function createChatLine() {
+    const chatLine = document.createElement("div")
+    chatLine.id = "chatLine"
+    chatLine.className = "chatLines"
+    document.getElementById("chat").appendChild(chatLine);
+    return chatLine
+}
+
+function createChannelLine(chatLog) {
+    const channelLine = document.createElement("div")
+    channelLine.id = "channelLine"
+    channelLine.className = "channelLines"
+    channelLine.textContent = chatLog.channel + "'s chat"
+    document.getElementById("chat").appendChild(channelLine)
+    return channelLine
+}
+
+function createUserNameSpan(chatLog, chatLine) {
+    const usernameSpan = document.createElement("span");
+    usernameSpan.id = chatLog.id;
+    usernameSpan.textContent = chatLog.username;
+    usernameSpan.style.color = chatLog.color;
+    usernameSpan.style.fontWeight = "bold";
+    chatLine.appendChild(usernameSpan)
+    console.log(usernameSpan)
+}
+
+function createMessageSpan(chatLog) {
+    const messageSpan = document.createElement("span");
+    messageSpan.className = "messages";
+    messageSpan.style.color = "#979799";
+    messageSpan.style.fontWeight = "normal";
+    const messagesArray = [];
+    postTwitchEmotes(chatLog, messagesArray)
+    messageSpan.innerHTML = ": " + messagesArray.join('')
+    console.log(chatLog.message)
+    return messageSpan
+}
+
+function postTwitchEmotes(chatLog, messagesArray) {
+    const messages = chatLog.message
+    for (const i in messages) {
+        console.log("each message: " + messages[i])
+        if (messages[i].type === "emote") {
+            messagesArray.push("<img src=https://static-cdn.jtvnw.net/emoticons/v1/" + messages[i].id + "/1.0></img>")
+        }
+        if (messages[i].type === "text") {
+            messagesArray.push(messages[i].text)
+        }
+    }
 }
